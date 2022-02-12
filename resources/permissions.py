4696fdbb9f466a0c2ce40429fe02cf3a39ce7b78
EC2 Default User <ec2-user@ip-10-16-10-182.us-west-2.compute.internal>
2022-02-12 20:32:58 +0000

    after setup

--- resources/permissions.py
+++ resources/permissions.py
@@ -1,11 +1,11 @@
 '''
-	You must replace <FMI> with your bucket name
+	You must replace c11284a125436l1633786t1w949735717612-s3bucket-gu3a6f9mz48g with your bucket name
 '''
 import boto3
 import json
 
 S3API = boto3.client("s3", region_name="us-west-2") 
-bucket_name = "<FMI>"
+bucket_name = "c11284a125436l1633786t1w949735717612-s3bucket-gu3a6f9mz48g"
 
 policy_file = open("/home/ec2-user/environment/resources/public_policy.json", "r")
 
